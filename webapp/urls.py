06e2594996ccae2bbaf7a84ac685ab9b2e97b70a
Farhatjan <faralost@gmail.com>
2022-02-05 14:14:36 +0600

    added updating choices of polls

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from webapp.views.choice_views import PollChoiceAddView
+from webapp.views.choice_views import PollChoiceAddView, PollChoiceUpdate
 from webapp.views.poll_views import PollsIndexView, PollDetailView, PollAddView, PollUpdateView, PollDeleteView
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('poll/<int:pk>/update/', PollUpdateView.as_view(), name='poll_update'),
     path('poll/<int:pk>/delete/', PollDeleteView.as_view(), name='poll_delete'),
     path('poll/<int:pk>/choices/add/', PollChoiceAddView.as_view(), name='poll_choice_add'),
+    path('task/<int:pk>/update/', PollChoiceUpdate.as_view(), name='poll_choice_update'),
 ]
